dbb4a118ae73c60ec513e99f27315c9bd2dfed24
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-06 18:44:12 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,15 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backers, :projects, :add_backer
   attr_reader :backers
   
   def initialize(title)
     @title = title
     @backers = []
+  end 
+  
+  def add_backer(project)
+    @backed_projects << project
+    project.backers << self 
   end 
   
 end 
